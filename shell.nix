1734ba95a8907e47fe276c1643399b20f2756de0
Space Banana <tomasbessa@protonmail.com>
2023-11-14 11:27:19 +0000

    Shell.nix change

--- shell.nix
+++ shell.nix
@@ -9,8 +9,9 @@ pkgs.mkShell {
   shellHook = ''
 clear && echo "Yakumo dev environment created
 The following shortcuts were created:
-  * $yakumo - launches yakumo.jar in the current directory
-  * $getyakumo - downloads the latest Yakumo from the repository's main branch
+  * yakumo - launches yakumo.jar in the current directory
+  * getyakumo - downloads the latest Yakumo from the repository's main branch
+Type the shortcuts with the character '$' at the start
 "
 '';
   yakumo = "scala yakumo.jar";
